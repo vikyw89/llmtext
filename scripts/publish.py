dbd0028611b5362f900249b626d10d88428850f0
vikyw89 <vikyw89@gmail.com>
2024-06-08 17:54:22 +0800

    chore: published and added script automaton

--- scripts/publish.py
+++ scripts/publish.py
@@ -2,3 +2,5 @@ def run():
     import subprocess
 
     subprocess.run(args="poetry version patch", shell=True)
+    subprocess.run(args="poetry build", shell=True)
+    subprocess.run(args="poetry publish", shell=True)
