e736adcd9b6cd86ca1d91df33b75eaca3bc75a29
vikyw89 <vikyw89@gmail.com>
2024-06-15 11:58:28 +0800

    chore: updated build script

--- llmtext/scripts/publish.py
+++ llmtext/scripts/publish.py
@@ -1,6 +1,4 @@
 def run():
     import subprocess
 
-    subprocess.run(args="poetry version patch", shell=True)
-    subprocess.run(args="poetry build", shell=True)
-    subprocess.run(args="poetry publish", shell=True)
+    subprocess.run(args="poetry publish --build", shell=True)
