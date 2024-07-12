02af6ddbca4657d4473de91fd483c8302ef35a8a
vikyw89 <vikyw89@gmail.com>
2024-07-12 17:43:17 +0800

    feat: versioning and more test

---
+++ tests/text/test_text_gemini.py
@@ -0,0 +1,97 @@
+import os
+from typing import Annotated, AsyncIterable
+from pydantic import BaseModel, Field
+from llmtext.text import Text
+import asyncio
+from openai import AsyncOpenAI
+
+
+def test_gemini_arun():
+
+    async def arun():
+
+        llm = Text(
+            text="What is the capital of France ?",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="google/gemini-flash-1.5",
+        )
+        res = await llm.arun()
+        assert res is not None
+
+    asyncio.run(arun())
+
+
+def test_gemini_stream():
+
+    async def astream():
+        llm = Text(
+            text="What is the capital of France ?",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="google/gemini-flash-1.5",
+        )
+        async for res in llm.astream():
+            assert isinstance(res, str)
+
+    asyncio.run(astream())
+
+
+def test_gemini_structured_extraction():
+
+    async def astructured_extraction():
+        llm = Text(
+            text="Paris is the capital of France.",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="google/gemini-flash-1.5",
+        )
+        class ExtractedData(BaseModel):
+            name: Annotated[str, Field(description="Name of the city")]
+            description: Annotated[str, Field(description="Description of the city")]
+
+        res = await llm.astructured_extraction(
+            output_class=ExtractedData,
+        )
+
+        assert isinstance(res, ExtractedData)
+
+    asyncio.run(astructured_extraction())
+
+
+def test_gemini_astream_structured_extraction():
+
+    llm = Text(
+        text="Paris is the capital of France.",
+        openai_client=AsyncOpenAI(
+            api_key=os.getenv("OPENROUTER_API_KEY", ""),
+            base_url=os.getenv("OPENROUTER_BASE_URL"),
+        ),
+        openai_model="google/gemini-flash-1.5",
+    )
+
+    class City(BaseModel):
+        name: Annotated[str, Field(description="Name of the city")]
+        description: Annotated[str, Field(description="Description of the city")]
+
+    class ExtractedData(BaseModel):
+        cities: Annotated[list[City], Field(description="Cities")]
+
+    async def arun():
+        res = await llm.astream_structured_extraction(
+            output_class=ExtractedData,
+        )
+
+        assert isinstance(res, AsyncIterable)
+
+        async for chunk in res:
+            assert isinstance(chunk, ExtractedData)
+            print(chunk.model_dump())
+
+    asyncio.run(arun())
---
+++ tests/text/test_text_haiku.py
@@ -0,0 +1,98 @@
+import os
+from typing import Annotated, AsyncIterable
+from pydantic import BaseModel, Field
+from llmtext.text import Text
+import asyncio
+from openai import AsyncOpenAI
+
+
+def test_haiku_arun():
+
+    async def arun():
+
+        llm = Text(
+            text="What is the capital of France ?",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="anthropic/claude-3-haiku",
+        )
+        res = await llm.arun()
+        assert res is not None
+
+    asyncio.run(arun())
+
+
+def test_haiku_stream():
+
+    async def astream():
+        llm = Text(
+            text="What is the capital of France ?",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="anthropic/claude-3-haiku",
+        )
+        async for res in llm.astream():
+            assert isinstance(res, str)
+
+    asyncio.run(astream())
+
+
+def test_haiku_structured_extraction():
+
+    async def astructured_extraction():
+        llm = Text(
+            text="Paris is the capital of France.",
+            openai_client=AsyncOpenAI(
+                api_key=os.getenv("OPENROUTER_API_KEY", ""),
+                base_url=os.getenv("OPENROUTER_BASE_URL"),
+            ),
+            openai_model="anthropic/claude-3-haiku",
+        )
+
+        class ExtractedData(BaseModel):
+            name: Annotated[str, Field(description="Name of the city")]
+            description: Annotated[str, Field(description="Description of the city")]
+
+        res = await llm.astructured_extraction(
+            output_class=ExtractedData,
+        )
+
+        assert isinstance(res, ExtractedData)
+
+    asyncio.run(astructured_extraction())
+
+
+def test_haiku_astream_structured_extraction():
+
+    llm = Text(
+        text="Paris is the capital of France.",
+        openai_client=AsyncOpenAI(
+            api_key=os.getenv("OPENROUTER_API_KEY", ""),
+            base_url=os.getenv("OPENROUTER_BASE_URL"),
+        ),
+        openai_model="anthropic/claude-3-haiku",
+    )
+
+    class City(BaseModel):
+        name: Annotated[str, Field(description="Name of the city")]
+        description: Annotated[str, Field(description="Description of the city")]
+
+    class ExtractedData(BaseModel):
+        cities: Annotated[list[City], Field(description="Cities")]
+
+    async def arun():
+        res = await llm.astream_structured_extraction(
+            output_class=ExtractedData,
+        )
+
+        assert isinstance(res, AsyncIterable)
+
+        async for chunk in res:
+            assert isinstance(chunk, ExtractedData)
+            print(chunk.model_dump())
+
+    asyncio.run(arun())
